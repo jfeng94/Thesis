0098fb0afab8b3718db299b24632795681b4b82e
Jerry Feng <jfeng94@gmail.com>
2016-04-22 20:58:22 -0700

    Started data analysis stuff

---
+++ Thesis/Assets/Scripts/DataAnalysis/DataAnalysis.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class DataAnalysis {
+	List<DataGlob> Users = new List<DataGlob>();
+	public DataAnalysis() {
+		LoadDataGlobs();
+	}
+
+	private void LoadDataGlobs() {
+		return;
+	}
+}
